86e955e0b16b22ab294edec455381b4bf05db768
Pablo Freitas Machado <pablo.fmachado92@gmail.com>
2019-08-15 17:30:43 -0300

    Added React Navigation , React Gestures, and React Reanimated

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +27,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default createAppContainer(AppNavigator);
